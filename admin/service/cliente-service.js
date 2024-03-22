95e524b658cc27f52acb420866f0327c7848b1a5
Teodoro <149595409+sanallite@users.noreply.github.com>
2024-03-22 11:55:21 -0300

    Cadastro de clientes feito.

--- admin/service/cliente-service.js
+++ admin/service/cliente-service.js
@@ -5,6 +5,22 @@ export const listaClientes = () => {
     )
 }
 
+export const criarCliente = (nome, email) => {
+    return fetch('http://localhost:3000/profile', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            nome: nome,
+            email: email
+        })
+    }).then( resposta => {
+            return resposta.body
+        } )
+}
+
 export const clientService = {
-    listaClientes
+    listaClientes,
+    criarCliente
 }
